ba2c9daee7c5e84efd9645b563c7b025438f46b2
Jaime Ramirez <jaime.ramirez@redhat.com>
2022-06-21 15:20:31 +0200

    fix tests

--- functions-eda/solutions/battery-checker/test/integration.js
+++ functions-eda/solutions/battery-checker/test/integration.js
@@ -1,59 +1,39 @@
 'use strict';
-
 const { start } = require('faas-js-runtime');
 const request = require('supertest');
 
 const func = require('..').handle;
 const test = require('tape');
 
+const Spec = {
+  version: 'ce-specversion',
+  type: 'ce-type',
+  id: 'ce-id',
+  source: 'ce-source'
+};
+
+const data = {
+  name: 'tiger',
+  customerId: '01234'
+}
+
 const errHandler = t => err => {
   t.error(err);
   t.end();
 };
 
-test('Integration: handles an HTTP GET', t => {
+test('Integration: handles a valid event', t => {
   start(func).then(server => {
-    t.plan(2);
-    request(server)
-      .get('/?name=tiger')
-      .expect(200)
-      .expect('Content-Type', /json/)
-      .end((err, res) => {
-        t.error(err, 'No error');
-        t.deepEqual(res.body, { query: { name: 'tiger' } });
-        t.end();
-        server.close();
-      });
-  }, errHandler(t));
-});
-
-test('Integration: handles an HTTP POST', t => {
-  start(func).then(server => {
-    t.plan(2);
+    t.plan(0);
     request(server)
       .post('/')
-      .send({ name: 'tiger' })
+      .send(data)
+      .set(Spec.id, '01234')
+      .set(Spec.source, '/test')
+      .set(Spec.type, 'MyTest')
+      .set(Spec.version, '1.0')
       .expect(200)
-      .expect('Content-Type', /json/)
-      .end((err, res) => {
-        t.error(err, 'No error');
-        t.deepEqual(res.body, { name: 'tiger' });
-        t.end();
-        server.close();
-      });
-  }, errHandler(t));
-});
-
-test('Integration: responds with error code if neither GET or POST', t => {
-  start(func).then(server => {
-    t.plan(1);
-    request(server)
-      .put('/')
-      .send({ name: 'tiger' })
-      .expect(200)
-      .expect('Content-Type', /json/)
-      .end((err, res) => {
-        t.deepEqual(res.body, { message: 'Route PUT:/ not found', error: 'Not Found', statusCode: 404 });
+      .end((err, result) => {
         t.end();
         server.close();
       });
--- functions-eda/solutions/battery-checker/test/unit.js
+++ functions-eda/solutions/battery-checker/test/unit.js
@@ -8,22 +8,15 @@ const fixture = { log: { info: console.log } };
 test('Unit: handles an HTTP GET', t => {
   t.plan(1);
   // Invoke the function, which should complete without error.
-  const result = func({ ...fixture, method: 'GET', query: { name: 'tiger' } });
-  t.deepEqual(result, { query: { name: 'tiger' } });
+  const result = func({ ...fixture, method: 'GET', query: { name: 'tiger' } }, { type: "MyEvent"});
+  t.deepEqual(result, "OK");
   t.end();
 });
 
 test('Unit: handles an HTTP POST', t => {
   t.plan(1);
   // Invoke the function, which should complete without error.
-  const result = func({ ...fixture, method: 'POST', body: { name: 'tiger' } });
-  t.deepEqual(result, { body: { name: 'tiger' } });
-  t.end();
-});
-
-test('Unit: responds with error code if neither GET or POST', t => {
-  t.plan(1);
-  const result = func(fixture);
-  t.deepEqual(result, { statusCode: 405, statusMessage: 'Method not allowed' });
+  const result = func({ ...fixture, method: 'POST', body: { name: 'tiger' } }, { type: "MyEvent"});
+  t.deepEqual(result, "OK");
   t.end();
 });
