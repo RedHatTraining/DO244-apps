1bdcca68a6c1d7be2766457bf10e04e2fcda6b21
Jaime Ramirez <jaime.ramirez@redhat.com>
2022-07-05 09:25:04 +0200

    tweak code comments

--- functions-introduction/solutions/weather-python/func.py
+++ functions-introduction/solutions/weather-python/func.py
@@ -4,22 +4,22 @@ from methods import kelvin_to_celsius, kelvin_to_farhenheit, read_cities_weather
 
 def main(context: Context):
 
-    # TODO: Parse city_name parameter from query string
+    # Parse city_name parameter from query string
     city_name = context.request.args.get('city_name')
 
-    # TODO: Read cities weather data
+    # Read cities weather data
     json_data = read_cities_weather()
 
-    # TODO: Get weather by city_name, or return an error
+    # Get weather by city_name, or return an error
     try:
         city = json_data['city'][city_name]
     except KeyError:
         return {"result": "City cannot be found!"}, 404
 
-    # TODO: Get kelvin temperature from city
+    # Get kelvin temperature from city
     temp_kelvin = city['main']['temp']
 
-    # TODO: Convert kelvins to Celisus and Fahrenheit
+    # Convert kelvins to Celisus and Fahrenheit
     temp_celsius = kelvin_to_celsius(temp_kelvin)
     temp_fahrenheit = kelvin_to_farhenheit(temp_kelvin)
 
