6031cc48707508cd16caee098d89b5d424ad97fc
Jaime Ramirez <jaime.ramirez@redhat.com>
2022-07-08 17:14:17 +0200

    simplify read_weather

--- functions-introduction/solutions/city-weather-quarkus/src/main/java/com/redhat/training/weather/Methods.java
+++ functions-introduction/solutions/city-weather-quarkus/src/main/java/com/redhat/training/weather/Methods.java
@@ -9,25 +9,11 @@ import org.json.simple.parser.ParseException;
 public class Methods {
     ClassLoader classLoader = getClass().getClassLoader();
     String jsonFileName = "cities.json";
-    Object weatherData = new Object();
-    File jsonResourceFile;
 
     public Object read_weather() throws FileNotFoundException, IOException, ParseException, URISyntaxException {
-        InputStream inputStream = classLoader.getResourceAsStream( jsonFileName );
-        if ( inputStream == null ) {
-            throw new IllegalArgumentException( "JSON file not found! " + jsonFileName );
-        } else {
-            // Fetching URL of the JSON file.
-            URL resource = classLoader.getResource( jsonFileName );
-            if ( resource == null ) {
-                throw new IllegalArgumentException( "JSON file not found! " + jsonFileName );
-            } else {
-                // Creating a new file using url
-                jsonResourceFile = new File( resource.toURI() );
-                weatherData = new JSONParser().parse( new FileReader( jsonResourceFile.toPath().toString() ) );
-            }
-        }
-        return weatherData;
+        URL fileResource = classLoader.getResource( jsonFileName );
+        File jsonResourceFile = new File( fileResource.toURI() );
+        return new JSONParser().parse( new FileReader( jsonResourceFile.toPath().toString() ) );
     }
 
     public double kelvin_to_celsius( double temp ) {
